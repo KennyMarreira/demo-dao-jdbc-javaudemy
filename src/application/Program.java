17797564280d43a4f62dad45635b5cb84eac6cd7
KennyMarreira <kennymarreira@hotmail.com>
2023-03-16 15:58:43 -0300

    Reusing instatiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
+		
+		System.out.println("=== TEST 1: findById ===");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
